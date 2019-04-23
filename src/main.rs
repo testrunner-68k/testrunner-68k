7e4b06b37a511ce53f17a76ec0fee6d30507a12c
Mikael Kalms <mikael@kalms.org>
2019-04-23 15:46:21 +0200

    Created testcase which enumerates tests

--- src/main.rs
+++ src/main.rs
@@ -31,12 +31,14 @@ fn get_test_cases_for_hunk(hunk: &Hunk, start_address: &u32) -> Vec<TestCase> {
     let mut test_cases = Vec::new();
     match hunk.hunk_type {
         HunkType::Code | HunkType::Data => {
-            match hunk.symbols {
+            match hunk.symbols.as_ref() {
                 Some(symbols) => {
-                    test_cases = symbols.iter().filter(|symbol| symbol.name.starts_with("test_")).map(|symbol| TestCase { name: symbol.name, entrypoint: start_address + symbol.offset }).collect();
-                }
+                    test_cases = symbols.iter().filter(|symbol| symbol.name.starts_with("test_")).map(|symbol| TestCase { name: symbol.name.clone(), entrypoint: start_address + symbol.offset }).collect();
+                },
+                None => {}
             }
-        }
+        },
+        HunkType::Bss => {}
     }
 
     return test_cases;
@@ -64,4 +66,15 @@ fn main() {
 //    dbg!(&hunks);
     let hunk_layout = layout_hunks(&hunks);
     let test_cases = get_test_cases(&hunks, &hunk_layout);
+}
+
+#[test]
+fn test_cases_enumerate_successfully() {
+    let hunks = HunkParser::parse_file("testdata/test.test_cases.amiga.exe").unwrap();
+    let hunk_layout = layout_hunks(&hunks);
+    let test_cases = get_test_cases(&hunks, &hunk_layout);
+    assert_eq!(3, test_cases.len());
+    assert_eq!("test_Module1_Case1", test_cases[0].name);
+    assert_eq!("test_Module2_Case2", test_cases[1].name);
+    assert_eq!("test_Module1_Case2", test_cases[2].name);
 }
---
+++ testdata/test.test_cases.amiga.s
@@ -0,0 +1,19 @@
+
+        section code,code
+
+start
+        nop
+        nop
+
+test_Module1_Case1
+        nop
+
+test_Module2_Case2
+        nop
+
+        section data,data
+
+        nop
+test_Module1_Case2
+        nop
+irrelevant_label
