ace35e2b891e00d2f20fa0eb1f12cba541d09c62
Mikael Kalms <mikael@kalms.org>
2019-04-24 13:09:19 +0200

    WIP test fails during execution

--- src/musashi.rs
+++ src/musashi.rs
@@ -1,6 +1,7 @@
 #![allow(nonstandard_style)]
 #![allow(dead_code)]
 
+use std::ptr;
 use std::sync::Mutex;
 
 include!(concat!(env!("OUT_DIR"), "/musashi.bindings.rs"));
@@ -55,10 +56,25 @@ pub extern fn m68k_write_memory_32(address: u32, value: u32) {
     write_memory_byte(address + 3, value);
 }
 
-
 #[test]
-fn start_musashi() {
+fn run_musashi() {
     unsafe {
         m68k_init();
+        m68k_write_memory_32(0, 0xf000);
+        m68k_write_memory_32(4, 0x1000);
+
+        m68k_write_memory_16(0x1000, 0x7005);   // MOVEQ #5,d0
+        m68k_write_memory_16(0x1002, 0x60fe);   // BRA.S *
+
+        m68k_pulse_reset();
+        m68k_execute(1000); // Test failure during execution
+
+        // // let mut context: Vec<u8> = Vec::new();
+        // // context.reserve(m68k_context_size() as usize);
+        // // m68k_get_context(&context);
+
+        // let d0 = m68k_get_reg(ptr::null_mut(), m68k_register_t_M68K_REG_D0);
+        // dbg!(&d0);
+        // //assert_eq!(5u32, d0);
     }
 }
