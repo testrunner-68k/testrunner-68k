7238cbdc15ad70faa4c450be0d6e15fd6f9d621b
Mikael Kalms <mikael@kalms.org>
2019-04-24 12:51:57 +0200

    Added successful & failing test cases

---
+++ testdata/test.failed_test_case.amiga.s
@@ -0,0 +1,10 @@
+
+        section code,code
+
+start
+		illegal
+		rts
+
+test_TestModule_failedCase
+		moveq	#0,d0
+		rts
---
+++ testdata/test.successful_test_case.amiga.s
@@ -0,0 +1,10 @@
+
+        section code,code
+
+start
+		illegal
+		rts
+
+test_TestModule_successfulCase
+		moveq	#1,d0
+		rts
