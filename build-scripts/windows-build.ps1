d8eb186ec8237adf21367ddb3e1d69d3d8dfc475
Mikael Kalms <mikael@kalms.org>
2019-05-11 20:39:55 +0200

    Don't do Powershell arg parsing for native Windows app invocations

--- build-scripts/windows-build.ps1
+++ build-scripts/windows-build.ps1
@@ -1,19 +1,19 @@
 $ErrorActionPreference = "Stop"
 
 # Build Musashi in debug & release configurations
-tundra2 win32-msvc-debug-default win32-msvc-release-default
+tundra2 --% win32-msvc-debug-default win32-msvc-release-default
 if ($LASTEXITCODE -ne 0) { throw "Building Musashi failed with exit code $LASTEXITCODE" }
 
 # Build & run testrunner-68k tests
-cargo test
+cargo --% test
 if ($LASTEXITCODE -ne 0) { throw "Building/running testrunner-68k tests failed with exit code $LASTEXITCODE" }
 
 # Build testrunner-68k executable in debug config
-cargo build
+cargo --% build
 if ($LASTEXITCODE -ne 0) { throw "Building testrunner-68k in debug configuration failed with exit code $LASTEXITCODE" }
 
 # Build testrunner-68k executable in release config
-cargo build --release
+cargo --% build --release
 if ($LASTEXITCODE -ne 0) { throw "Building testrunner-68k in release configuration failed with exit code $LASTEXITCODE" }
 
 # Include testrunner-68k executable with deploy
