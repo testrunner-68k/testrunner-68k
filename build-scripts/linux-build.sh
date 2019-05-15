673720b8b762055adfa04248973b5a874ca47fe3
Mikael Kalms <mikael@kalms.org>
2019-05-15 08:29:01 +0200

    Build ID embedded into Cargo.toml for Linux builds

--- build-scripts/linux-build.sh
+++ build-scripts/linux-build.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+BUILD_ID="$1"
+
+# Inject build ID into Cargo.toml, if it exists
+if [[ -n "$BUILD_ID" ]]; then
+    sed -i -e "s/^version *= *\"0.0.0-localbuild\"$/version = \"0.0.0-build${BUILD_ID}\"/" Cargo.toml
+fi
 
 # Build Musashi
 tundra2 linux-gcc-debug-default linux-gcc-release-default
