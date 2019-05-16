ee7d82ab2318b06d81e1584414aa3db7465084d9
Mikael Kalms <mikael@kalms.org>
2019-05-16 18:34:26 +0200

    Build ID embedded into Windows builds
    Renamed Windows build to match standard naming
    
    Bugfixed null build ID handling in Windows script

--- build-scripts/windows-build.ps1
+++ build-scripts/windows-build.ps1
@@ -1,9 +1,16 @@
 
 Param(
-    [String] $BuildId
+    [Parameter(Mandatory=$false)] [String] $BuildId
 )
 
 $ErrorActionPreference = "Stop"
+
+# Inject build ID into Cargo.toml, if it has been specified
+echo "BuildId: .${BuildId}."
+if ($BuildId -ne "")
+{
+    (Get-Content Cargo.toml) -replace '^version *= *".*"$', "version = `"0.0.0-build${BuildId}`"" | Out-File -Encoding UTF8 Cargo.toml
+}
 
 # Build Musashi in debug & release configurations
 tundra2 win32-msvc-debug-default win32-msvc-release-default
@@ -21,11 +28,11 @@ if ($LASTEXITCODE -ne 0) { throw "Building testrunner-68k in debug configuration
 cargo build --release
 if ($LASTEXITCODE -ne 0) { throw "Building testrunner-68k in release configuration failed with exit code $LASTEXITCODE" }
 
-if ($BuildId -ne $null)
+if ($BuildId -ne "")
 {
     # Package up testrunner-68k windows binaries for deploy
     if (Test-Path deploy) { rd -recurse deploy }
     md deploy
-    7z a deploy\testrunner-68k-${BuildId}-windows-binaries.zip .\target\release\testrunner-68k.exe
+    7z a deploy\testrunner-68k-build${BuildId}-windows-binaries.zip .\target\release\testrunner-68k.exe
     if ($LASTEXITCODE -ne 0) { throw "Creating windows binaries zip archive failed with exit code $LASTEXITCODE" }
 }
