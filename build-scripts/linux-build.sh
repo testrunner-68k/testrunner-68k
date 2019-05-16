202d28354703d3588cdecbc5d97d18ffdc3617fb
Mikael Kalms <mikael@kalms.org>
2019-05-16 22:46:42 +0200

    Create deb package, deploy to S3, include in release

--- build-scripts/linux-build.sh
+++ build-scripts/linux-build.sh
@@ -18,3 +18,10 @@ cargo build
 
 # Build testrunner-68k executable in release config
 cargo build --release
+
+if [[ -n "$BUILD_ID" ]]; then
+    # Package up application as .deb package
+    cargo deb --no-build
+    mkdir -p deploy
+    cp target/debian/testrunner-68k_0.0.0-build${BUILD_ID}_amd64.deb deploy/
+fi
