912f670d1a47661d4535263a4d1b0e4361d83258
Mikael Kalms <mikael@kalms.org>
2019-08-08 21:01:17 +0000

    Enable per-line tracing for Powershell scripts

--- build-scripts/windows-run-travis.ps1
+++ build-scripts/windows-run-travis.ps1
@@ -3,6 +3,9 @@ Param(
     [Parameter(Mandatory=$false)] [String] $Version
 )
 
+# Print each line of PowerShell script before it is executed
+Set-PSDebug -Trace 1
+
 & $PSScriptRoot\windows-install-build-tools.ps1
 & $PSScriptRoot\windows-install-package-tools.ps1
 & $PSScriptRoot\windows-build.ps1 $Version
