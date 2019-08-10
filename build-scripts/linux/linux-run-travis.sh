5de15d88e29f3bd5d48864123bee7bab412727a8
Mikael Kalms <mikael@kalms.org>
2019-08-10 22:29:49 +0200

    Added progress markers to text output

--- build-scripts/linux/linux-run-travis.sh
+++ build-scripts/linux/linux-run-travis.sh
@@ -6,5 +6,12 @@ VERSION="$1"
 
 SCRIPT_DIRECTORY="$(dirname -- "$0")"
 
+echo "**************** INSTALLING BUILD & PACKAGING TOOLS ****************"
+
 ${SCRIPT_DIRECTORY}/linux-install.sh
+
+echo "*********************** BUILDING & PACKAGING ***********************"
+
 ${SCRIPT_DIRECTORY}/linux-build.sh "$VERSION"
+
+echo "******************************* DONE *******************************"
