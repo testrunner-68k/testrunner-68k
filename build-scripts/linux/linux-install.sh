2c74bf26ab844d8490516f4e1db1630c7f508a60
Mikael Kalms <mikael@kalms.org>
2019-08-10 22:30:57 +0200

    Test building .deb archive using the supplied version of cargo-deb

--- build-scripts/linux/linux-install.sh
+++ build-scripts/linux/linux-install.sh
@@ -15,5 +15,6 @@ fi
 
 # Install cargo 'deb' subcommand
 # If the subcommand is already installed, uninstall first; this ensures that the latest version is used
-if [[ -n `cargo install --list | grep "cargo-deb"` ]]; then cargo uninstall cargo-deb; fi
+#if [[ -n `cargo install --list | grep "cargo-deb"` ]]; then cargo uninstall cargo-deb; fi
+cargo install --list
 cargo install cargo-deb
