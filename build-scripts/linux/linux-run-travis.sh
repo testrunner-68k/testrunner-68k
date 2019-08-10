242f42a7413717c2c62c1abb0bf129d7a65c397e
Mikael Kalms <mikael@kalms.org>
2019-08-10 22:22:09 +0200

    Protect against paths with spaces and dashes

--- build-scripts/linux/linux-run-travis.sh
+++ build-scripts/linux/linux-run-travis.sh
@@ -4,7 +4,7 @@ set -e
 
 VERSION="$1"
 
-SCRIPT_DIRECTORY="$(dirname $0)"
+SCRIPT_DIRECTORY="$(dirname -- "$0")"
 
 ${SCRIPT_DIRECTORY}/linux-install.sh
 ${SCRIPT_DIRECTORY}/linux-build.sh "$VERSION"
